8af1292024db7ae0dc5525d84a45d6f5f8480f8a
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-09-24 00:02:55 -0300

    Primeiro commit. Criar projeto e pacotes.

---
+++ src/test/java/com/marcioviana/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcioviana.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
