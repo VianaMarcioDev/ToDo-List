1b186f99848a62ad170752070b914065523e0529
Márcio de Freitas Viana <marcioviana.developer@gmail.com>
2023-09-24 17:26:25 -0300

    Criar teste CreateSuccess

--- src/test/java/com/marcioviana/todolist/ToDoListApplicationTests.java
+++ src/test/java/com/marcioviana/todolist/ToDoListApplicationTests.java
@@ -1,13 +1,47 @@
 package com.marcioviana.todolist;
 
+import com.marcioviana.todolist.entity.ToDo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
+@AutoConfigureWebTestClient
 class ToDoListApplicationTests {
 
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
-    void contextLoads() {
+    void testCreateToDoSuccess() {
+        var toDo = new ToDo("fazer compras","ir ao supermercado para fazer as compras do mês", false, 3 );
+
+        webTestClient
+                .post()
+                .uri("/todos")
+                .bodyValue(toDo)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$").isArray()
+                .jsonPath("$.length()").isEqualTo(1)
+                .jsonPath("$[0].nome").isEqualTo(toDo.getNome())
+                .jsonPath("$[0].descricao").isEqualTo(toDo.getDescricao())
+                .jsonPath("$[0].realizado").isEqualTo(toDo.isRealizado())
+                .jsonPath("$[0].prioridade").isEqualTo(toDo.getPrioridade());
     }
 
+    @Test
+    void testCreateToDoFailure() {
+    }
+
+    public WebTestClient getWebTestClient() {
+        return webTestClient;
+    }
+
+    public void setWebTestClient(WebTestClient webTestClient) {
+        this.webTestClient = webTestClient;
+    }
 }
